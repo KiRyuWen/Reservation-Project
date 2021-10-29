a5bc69f3222c36965242196f28325e2e4642122b
Ting <88333551+martin81213@users.noreply.github.com>
2021-10-30 01:07:51 +0800

    Registration ,log in ,log out -

--- firstproject/firstproject/urls.py
+++ firstproject/firstproject/urls.py
@@ -20,11 +20,12 @@ from loginapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('',views.index),
-    path('index/',views.index),
-    path('login/',views.login),
-    path('logout/',views.logout),
-    #path('adduser/',views.adduser),
+    path('', views.index),
+    path('index/', views.index),
+    path('login/', views.login),
+    path('logout/', views.logout),
+    path('register/', views.rigister),
+    # path('adduser/',views.adduser),
 
-    #path('post/',views.post),
+    # path('post/',views.post),
 ]
